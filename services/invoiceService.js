ddf63bc446f09747fcc66764102de0a07a44b16e
arjungarg07 <arjun.garg.cse01@gmail.com>
2021-06-21 02:13:23 +0530

    feat: setup invoice service

---
+++ services/invoiceService.js
@@ -0,0 +1,10 @@
+// # All the business logic is here
+const { Op } = require("sequelize");
+const { Invoice } = require("../models");
+
+class Invoices {
+  constructor() {}
+
+}
+
+module.exports = new Invoices();
