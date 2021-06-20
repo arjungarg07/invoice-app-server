e97c3ef38672f10fdd551045f4bc1623d442ea0c
arjungarg07 <arjun.garg.cse01@gmail.com>
2021-06-21 02:12:05 +0530

    fix: db config

--- config/index.js
+++ config/index.js
@@ -1,8 +1,9 @@
 module.exports = {
     development: {
       host: "localhost",
+      username:"root",
       port: 3306,
-      password: "adminadmin",
+      password: "secretkey",
       pool: {
         max: 20,
         min: 5,
